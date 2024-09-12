f48288de702c1380c17f7c13bc7160e4b7d6bb32
Antariex <arielantequiera@gmail.com>
2024-09-11 23:49:58 -0300

    Planting a Tree project

---
+++ src/Tree.java
@@ -0,0 +1,20 @@
+//Introduce yourself to users and plant a tree for them!
+
+public class Tree {
+    public static void main(String args[]) {
+
+        //This method will introduce myself and print a tree to the screen
+
+        System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!\n");
+        System.out.println("I’m going to plant a tree today!\n");
+
+        System.out.println("     *");
+        System.out.println("    ***");
+        System.out.println("   *****");
+        System.out.println("  *******");
+        System.out.println(" *********");
+        System.out.println("    |||");
+
+        System.out.println("\nIs this a pine tree?");
+    }
+}
